35137928034a85206fa49acebb7cfdf39a3bc28e
João Paulo <jp_cbc@hotmail.com>
2022-07-13 00:35:05 -0300

    Primeiro component

---
+++ frontend/src/components/NotificationButton/index.tsx
@@ -0,0 +1,15 @@
+import './styles.css'
+
+function NotificationButton() {
+    return (
+        <div className="btn">
+            <img
+                src="/src/assets/img/notification-icon.svg"
+                alt="Notificar"
+                title="Notificar"
+            />
+        </div>
+    )
+}
+
+export default NotificationButton
